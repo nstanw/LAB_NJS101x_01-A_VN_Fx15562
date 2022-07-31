fef7757a8ee4a474a231b0fe530dbdf7e3da21ea
Tuan Anh <langthambca@gmail.com>
2022-07-31 23:03:38 +0700

    lab1.3

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.headers, req.method, req.url);
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My first Page</title></head>');
+  res.write('<body><h1>Hello form Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
 })
 
 server.listen(3000);
