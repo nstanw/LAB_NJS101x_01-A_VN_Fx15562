3783762431b85e8cce081f962c149ef6f169c45e
Tuan Anh <langthambca@gmail.com>
2022-07-31 23:35:15 +0700

    lab1.4

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers, req.method, req.url);
+  const url = req.url;
+  if (url === '/message') {
+
+
+    res.write('<html>');
+    res.write('<head><title>My first Page</title></head>');
+    res.write('<body><h1>Hello form Node.js Server!</h1></body>');
+    res.write('</html>');
+
+    return res.end();
+
+  }
 
   res.setHeader('Content-Type', 'text/html');
+
   res.write('<html>');
-  res.write('<head><title>My first Page</title></head>');
-  res.write('<body><h1>Hello form Node.js Server!</h1></body>');
+  res.write('<head><title>Enter Message</title></head>');
+  res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type = "submit">send</button></form> </body>');
   res.write('</html>');
+
   res.end();
 })
 
