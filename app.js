501bd388ab986f7f4c10d7ff087ffd0918c3f8ee
Tuan Anh <langthambca@gmail.com>
2022-08-06 11:19:31 +0700

    redirect 404

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.urlencoded({extended: false}));
 app.use(adminRouter);
 app.use(shopRouter);
 
+app.use((req, res) => { 
+    res.status(404).send('<h1>page not found</h1>');
+ })
 
 const server = http.createServer(app);
 
