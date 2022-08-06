af2b7651917ef5d268685af7dae3b61b54ef0a87
Tuan Anh <langthambca@gmail.com>
2022-08-06 10:39:22 +0700

    urlendecoded request

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('this alway run');
-next();
-})
+app.use(express.urlencoded({extended: false}));
 
 app.use('/add-product',(req, res, next) => {
     console.log('The Add product page!');
-    res.send('<h1>The Add product page</h1>')
+    res.send(`
+        <form action="product" method="POST">
+        <input name="title" type="text"></input>
+        <button type="submit">Submit</button>  
+        </form>
+    `);
+});
+
+app.post('/product', (req, res, next) => { 
+    console.log(req.body);
+    res.redirect('/') 
 });
 
 app.use('/',(req, res, next) => {
