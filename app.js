3c82406648548453afedccf381d5ea9de358ecae
Tuan Anh <langthambca@gmail.com>
2022-08-06 10:17:58 +0700

    xu li route

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In middleware!');
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('In another middlerware!');
-    res.send('<h1>Hello from express.js!</h1>')
+app.use((req, res, next)=>{
+    console.log('this alway run');
+next();
 })
 
+app.use('/add-product',(req, res, next) => {
+    console.log('The Add product page!');
+    res.send('<h1>The Add product page</h1>')
+});
+
+app.use('/',(req, res, next) => {
+    console.log('Hello from express.js!');
+    res.send('<h1>Hello from express.js!</h1>')
+})
 
 const server = http.createServer(app);
 
