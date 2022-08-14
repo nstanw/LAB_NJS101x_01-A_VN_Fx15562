c09a3c5c74e0d0480d6d05dd411b5ee714de3841
Tuan Anh <langthambca@gmail.com>
2022-08-14 15:45:42 +0700

    Dynamic content

--- router/shop.js
+++ router/shop.js
@@ -6,7 +6,13 @@ const rootDir = require('../util/path')
 const adminData = require('./admin')
 
 router.get('/',(req,res, next) => { 
-  res.render('shop.pug');
+  const products = adminData.products;
+  res.render('shop.pug',{
+      prods: products, 
+      docTitle: "Shop", 
+      // description: products[0].description,
+      // price: products[0].price,
+  });
  });
 
 module.exports = router
