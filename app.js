7a5f9ce3a759bf23b0bf8ed109c76600998d953c
Tuan Anh <langthambca@gmail.com>
2022-08-15 17:38:16 +0700

    Partial

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const http = require('http');
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
+
 const app = express();
-const ejs = require('ejs');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./router/admin');
-const shopRouter = require('./router/shop');
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
-app.use(shopRouter);
+app.use(shopRoutes);
 
-app.use((req, res) => {
-    res.status(404).render("404",{pageTitle: 'Page not found'});
-})
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
-const server = http.createServer(app);
-
-server.listen(3000);
-console.log("Starting server on port 3000... http://localhost:3000"); 
+app.listen(3000);
