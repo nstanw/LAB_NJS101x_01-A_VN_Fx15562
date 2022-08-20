688c136a6dfb3183f525b94eb7ef21e634bc04cc
T.Anh <langthambca@gmail.com>
2022-08-20 16:51:55 +0700

    nap data via model

--- controller/products.js
+++ controller/products.js
@@ -1,31 +1,30 @@
-const Products = require("../models/product");
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
-      pageTitle: 'Add Product',
-      path: '/admin/add-product',
-      formsCSS: true,
-      productCSS: true,
-      activeAddProduct: true
-    });
-  }
-
-exports.postAddProduct =  (req, res, next) => {
-    // products.push({ title: req.body.title });
-    const product = new Products(req.body.title);
-    product.save();
-    res.redirect('/');
-  }
-
-exports.getProducts =  (req, res, next) => {
-    const products = Products.fetchAll();
-    console.log(products);
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
   });
-}
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  });
+};
