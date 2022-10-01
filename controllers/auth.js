86c3ce3aa0168ff83d988a62b2d0259fbb87ff7c
T.Anh <langthambca@gmail.com>
2022-10-01 11:25:48 +0700

    Configuring Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,19 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn =
-    req.get('cookie')
-    .split(';')[1]
-    .trim()
-    .split('=')[1] == true;
+  // const isLoggedIn =
+  //   req.get('cookie')
+  //   .split(';')[1]
+  //   .trim()
+  //   .split('=')[1] == true;
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true');
+  // res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=31536000; Expires=Thu, 01 Jan 1970');
+  // https://viblo.asia/p/ban-da-hieu-ro-ve-http-cookie-djeZ1DvGKWz
+  res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=1; HttpOnly=true;');
   res.redirect('/');
 };
